c021255c878b159f6357e287c8cd29a70ce6648d
Chintan Shah <cgintan1998@gmail.com>
2015-04-02 16:11:30 -0600

    DU --> Venture
    Signed-off-by: Chintan Shah <cgintan1998@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -629,11 +629,11 @@ RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcomp
 # Rules for QCOM targets
 include $(BUILD_SYSTEM)/qcom_target.mk
 
-ifneq ($(DU_BUILD),)
+ifneq ($(VENTURE_BUILD),)
 
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/du/sepolicy/sepolicy.mk)
+$(eval include vendor/venture/sepolicy/sepolicy.mk)
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
